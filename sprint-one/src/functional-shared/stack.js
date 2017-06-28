2cb15fdc1b451ef305a28e96a5d986e611410701
JR Gray <wgray@tuta.io>
2017-06-28 14:59:19 -0700

    Complete functional shared stack data structure

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.currentSize = 0;
+  _.extend(storage, stackMethods);
+  return storage;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+
+	push : function(value) {
+	  	this[this.currentSize] = value;
+	  	this.currentSize++;
+	},				
+
+	pop : function() {
+    if (this.currentSize > 0) {
+      this.currentSize--;
+      var temp = this[this.currentSize];
+      delete this[this.currentSize];
+      return temp;
+    }
+	  return null;
+	},
+
+	size : function() { 
+		return this.currentSize; 
+	}
+
+};
+
 
 
