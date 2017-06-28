1d680f22a5773141f1c5b95ab3a9e2563cbf5916
JR Gray <wgray@tuta.io>
2017-06-28 15:06:04 -0700

    Complete prototypal stack data structure

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,54 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      delete storage[0];
+      for (var i = 1; i <= size; i++) {
+        Object.defineProperty(storage, i-1, Object.getOwnPropertyDescriptor(storage, i));
+      }
+      size--;
+    }
+    return null;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
 };
+
+
+
+  Object.defineProperty(storage, i-1,
+      Object.getOwnPropertyDescriptor(storage, i));
+
+
+// { 0 : 'bob', -> delete
+//   1 : 'jim', -> 
+//   2 : 'joe',
+//   3 : 'sue'}
+
+// size = 4
+
+// { 1 : 'jim', 
+//   2 : 'joe',
+//   3 : 'sue'}
+
+// dequeue 
+
+// { 0 : 'jim',
+//   1 : 'joe',
+//   2 : 'sue'}
+
+// size = 3
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(stackMethods);
+  storage.currentSize = 0;
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  push : function(value) {
+  	this[this.currentSize] = value;
+  	this.currentSize++;
+  },
 
+  pop : function() {
+  	if (this.currentSize > 0) {
+  		this.currentSize--;
+  		var lastElement = this[this.currentSize];
+  		delete this[this.currentSize];
+  		return lastElement;
+  	}
+  	return null;
+  },
+
+  size : function() {
+  	return this.currentSize;
+  }
+
+};
