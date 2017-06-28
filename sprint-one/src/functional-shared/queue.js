37d322f0a7d6dc4f80e6ee96b700d4b9e450a44f
JR Gray <wgray@tuta.io>
2017-06-28 15:44:51 -0700

    Complete functional shared queue data struct

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.currentSize = 0;
+  _.extend(obj, queueMethods);
+  return obj;
 };
+
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+	this[this.currentSize] = value;
+	this.currentSize++;
+};
 
+queueMethods.dequeue = function() {
+	if (this.currentSize > 0) {
+		var temp = this[0]
+		for (var i = 0; i < this.currentSize-1; i++) {
+			this[i] = this[i+1];
+		}
+		this.currentSize--;
+		return temp;
+	}
+	return null;
+};
+
+queueMethods.size = function() {
+	return this.currentSize;
+};
