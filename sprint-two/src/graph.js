69b854f515ba6dfe6696520184b6cef3a3c826b9
JR Gray <wgray@tuta.io>
2017-06-29 15:35:06 -0700

    Start pseudoclassical graph data struct

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,19 @@
 
 
 // Instantiate a new graph
-var Graph = function() {
+var Graph = function(node) {
+	this.nodes = [];
+	this.edges = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+	this.nodes.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+	
 };
 
 // Removes a node from the graph.
