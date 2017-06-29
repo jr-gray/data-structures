1969aa25ecc830579a33fda1175ac3b212db25cf
JR Gray <wgray@tuta.io>
2017-06-29 11:38:54 -0700

    Complete functional shared tree data struct

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,37 @@
 var Tree = function(value) {
+  
   var newTree = {};
   newTree.value = value;
-
+  newTree.children = [];
   _.extend(newTree, treeMethods);
-  newTree.children = [];  // fix me
-
   return newTree;
+
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-	this.children.push(value);
+	this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
 	
+	if (this.value === target) {
+		return true;
+	} 
+
+	if (this.children.length > 0) {
+		for (var i = 0; i < this.children.length; i++) {
+			if(this.children[i].contains(target)) {
+				return true;
+			}
+
+		}
+	}
+
+	return false;
+
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
