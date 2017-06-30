70c8a3bbf12040f472b402b709cbf4d747bb166c
JR Gray <wgray@tuta.io>
2017-06-30 14:55:24 -0700

    Complete all specs on hashTable
    - handles hash function collisions
    - adds and removes keys and values

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,37 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (Array.isArray(this._storage.get(index))) {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+	    if (this._storage.get(index)[i][0] === k) {
+	      this._storage.get(index)[i] = [k, v];
+	    }
+	  }
+  	this._storage.get(index).push([k, v]);
+  } else {
+    this._storage.set(index, [[k, v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  //if (Array.isArray(this._storage.get(index))) {
+
+  // go to index, iterate over arrays in that index looking for k === thatArray[0], return thatArray
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+  	if (this._storage.get(index)[i][0] === k) {
+  		return this._storage.get(index)[i][1];
+  	}
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+  	if (this._storage.get(index)[i][0] === k) {
+  		this._storage.get(index)[i].splice(i, 1);
+  	}
+  }
 };
 
 
