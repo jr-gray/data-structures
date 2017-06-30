1233c6f7d6b6984522ffc523abb199760e4945fc
JR Gray <wgray@tuta.io>
2017-06-29 19:29:37 -0700

    Add methods for set data struct
     - Add
     - contains
     - remove

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+	if (this._storage.length > 0) {
+		for (var i = 0; i < this._storage.length; i++) {
+			if (this._storage[i] === item) {
+				return true;
+			}
+		}
+	}
+	return false;
 };
 
 setPrototype.remove = function(item) {
+	if (this._storage.length > 0) {
+		for (var i = 0; i < this._storage.length; i++) {
+			if (this._storage[i] === item) {
+				this._storage.splice(i, 1);
+			}
+		}
+	}
+	return null;
 };
 
 /*
